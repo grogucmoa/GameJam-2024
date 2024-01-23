74a5dd82639aef9d41765163975a958612e0cfe9
grogucmoa <79045764+grogucmoa@users.noreply.github.com>
2024-01-23 20:07:39 +0100

    Syst de deplacement + Tour/tour
    J'ai enfin fini bordel

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,19 +11,47 @@ public class Player : MonoBehaviour
     public float vitesseDeplacement = 5f;
     private Vector3 positionClic;
     public bool isRunning = false;
+
+    //variable range
+    public float rayonDeplacement = 5f;
+    [SerializeField] private GameObject _range;
     
 
     void Update()
     {
         if(PlayerType == gameManager.ActiveTour)
         {
-            GetComponent<SpriteRenderer>().color = Color.green;
-            if (Input.GetMouseButtonDown(0)) // Clic gauche de la souris
+            _range.SetActive(true);
+            
+            if (gameManager.ActiveTour>= 1 && gameManager.ActiveTour <= 4) 
+            {
+                GetComponent<SpriteRenderer>().color = Color.green;
+            }
+
+            if (gameManager.ActiveTour>= 5 && gameManager.ActiveTour <= 8) 
+            {
+                GetComponent<SpriteRenderer>().color = Color.yellow;
+            }
+
+            if (gameManager.ActiveTour>=9 && gameManager.ActiveTour <= 12) 
+            {
+                GetComponent<SpriteRenderer>().color = Color.red;
+            }
+
+            if (gameManager.ActiveTour>= 13 && gameManager.ActiveTour <= 16) 
+            {
+                GetComponent<SpriteRenderer>().color = Color.blue;
+            }
+
+            if (Input.GetMouseButtonDown(0))
             {
                 positionClic = Camera.main.ScreenToWorldPoint(Input.mousePosition);
                 positionClic.z = 0;
-                isRunning = true;
 
+                if (Vector3.Distance(transform.position, positionClic) <= rayonDeplacement)
+                {
+                    isRunning = true;
+                }
             }
 
             if (transform.position != positionClic && isRunning == true)
@@ -33,7 +61,9 @@ public class Player : MonoBehaviour
                 }
             else if (isRunning == true)
                 {
+                    _range.SetActive(false);
                     gameManager.ActiveTour += 1;
+                    isRunning = false;
                 }
         }
     }
