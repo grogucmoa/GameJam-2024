b5a462c8a826f4390a04aaf0736fc1658b87a9f7
grogucmoa <79045764+grogucmoa@users.noreply.github.com>
2024-01-24 12:11:43 +0100

    Ajout Persos Finaux
    Ajout Persos Finaux

--- Assets/Scripts/bMove.cs
+++ Assets/Scripts/bMove.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Test : MonoBehaviour
+public class bMove : MonoBehaviour
 {
+    
+    
     // Start is called before the first frame update
     void Start()
     {
